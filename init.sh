a192121751e1c44c6317408e9d91d57d4c7fb889
nikadori_vagrant <vagrant@example.com>
2020-09-28 02:22:52 +0900

    update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,22 @@
 if [ -e /etc/centos-release ]; then
   sudo yum install -y vim
   sudo yum install -y tmux
+
+  # install and configure anyenv
+  git clone https://github.com/riywo/anyenv ~/.anyenv
+  
+  cat <<-"EOF" >> sample.txt
+		# setting anyenv
+		if [ -d $HOME/.anyenv ] then 
+		  export PATH="$HOME/.anyenv/bin:$PATH" 
+		  eval $(anyenv init -)" 
+		fi
+	EOF
+  >> sample.txt
+  
+	# install vim-plug 
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 # set .vimrc
